321b4c821a40f720f996b6a4773794b53cdf5223
Yanko Ivanov <yanko.ivanov@onmoon.org>
2022-05-31 18:59:07 +0300

    DRY generator testing

--- test/generators/monarch_migrate/install/install_generator_test.rb
+++ test/generators/monarch_migrate/install/install_generator_test.rb
@@ -1,24 +1,14 @@
 require "test_helper"
 
-require "fileutils"
 require "generators/monarch_migrate/install/install_generator"
-require "rails/generators/testing/assertions"
-require "rails/generators/testing/behaviour"
 
 module MonarchMigrate
   module Generators
     class InstallGeneratorTest < Minitest::Test
-      include Rails::Generators::Testing::Behaviour
-      include Rails::Generators::Testing::Assertions
-      include FileUtils
+      include Testing
 
       tests InstallGenerator
       destination File.expand_path("../tmp", __dir__)
-
-      def setup
-        super
-        prepare_destination
-      end
 
       def test_creates_a_migration
         ActiveRecord::Base.connection.stub(:data_source_exists?, false) do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,38 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 ENV["RAILS_ENV"] ||= "test"
 
-require "rails"
-require "rails/test_help"
+require "fake/application"
+require "fileutils"
 require "minitest/autorun"
 require "mocha/minitest"
-require "fake/application"
+require "rails"
+require "rails/generators/testing/assertions"
+require "rails/generators/testing/behaviour"
+require "rails/test_help"
 
 require "monarch_migrate"
+
+module MonarchMigrate
+  module Generators
+    module Testing
+      def self.included(other)
+        other.include Rails::Generators::Testing::Behaviour
+        other.include Rails::Generators::Testing::Assertions
+        other.include FileUtils
+      end
+
+      def setup
+        super
+        prepare_destination
+      end
+
+      def teardown
+        super
+        FileUtils.rm_rf(destination_root)
+      end
+    end
+  end
+end
 
 Rails.application.load_tasks
 
